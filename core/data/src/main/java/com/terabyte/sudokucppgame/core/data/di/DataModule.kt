e312196e69aed70cfa3a8866ef51d09e2fff28d6
artem <art.holodilov@gmail.com>
2026-07-23 10:05:53 +0300

    DataModule - Hilt module for DI in :core:data

---
+++ core/data/src/main/java/com/terabyte/sudokucppgame/core/data/di/DataModule.kt
@@ -0,0 +1,27 @@
+package com.terabyte.sudokucppgame.core.data.di
+
+import com.terabyte.sudokucppgame.core.data.jni.SudokuNative
+import com.terabyte.sudokucppgame.core.data.repository.SudokuRepositoryImpl
+import com.terabyte.sudokucppgame.core.domain.repository.SudokuRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun provideSudokuNative(): SudokuNative {
+        return SudokuNative()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSSudokuRepository(impl: SudokuRepositoryImpl): SudokuRepository {
+        return impl;
+    }
+}
