396f66d42e106c1ffa19292fafef9e0071df76b6
artem <art.holodilov@gmail.com>
2026-07-25 14:11:47 +0300

    Added androidx-startup-library to see how it works and optimizes cold start of the application

--- app/src/main/java/com/terabyte/sudokucppgame/application/MyApplication.kt
+++ app/src/main/java/com/terabyte/sudokucppgame/application/MyApplication.kt
@@ -6,16 +6,4 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class MyApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-        else {
-            // do nothing
-            // no logs in release version
-        }
-    }
-}
+class MyApplication : Application()
